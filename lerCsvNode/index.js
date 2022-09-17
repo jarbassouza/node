ff1e906659d54b2c12d409d21493ba3c290bdb63
Jarbas Pereira de Souza <jpsdelarue@gmail.com>
2022-09-17 18:26:57 -0300

    modificado

---
+++ lerCsvNode/index.js
@@ -0,0 +1,8 @@
+
+const fs = require ('fs')
+
+fs.readFile('arquivo.csv', (err, contents) => {
+    const lines = contents.toString().split('\n')
+    lines.forEach(line => console.log(line.split(',')))
+    console.log(lines)
+})
