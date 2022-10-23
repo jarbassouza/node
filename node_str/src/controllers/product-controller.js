2041a5d689c8f089f23144ee2b9d972ff864f6a7
Jarbas Pereira de Souza <jpsdelarue@gmail.com>
2022-10-23 20:16:38 -0300

    Up231020222016
    Repository

--- node_str/src/controllers/product-controller.js
+++ node_str/src/controllers/product-controller.js
@@ -2,15 +2,12 @@
 
 const mongoose = require("mongoose");
 const Product = mongoose.model("Product");
-const ValidationContract = require('../validators/fluent-validator')
+const ValidationContract = require("../validators/fluent-validator");
+const repository = require("../repositories/product-repository");
 
 exports.get = (req, res, next) => {
-  Product.find(
-    {
-      active: true,
-    },
-    "title price slug"
-  )
+repository
+    .get()
     .then((data) => {
       res.status(200).send(data);
     })
@@ -20,13 +17,8 @@ exports.get = (req, res, next) => {
 };
 
 exports.getBySlug = (req, res, next) => {
-  Product.findOne(
-    {
-      slug: req.params.slug,
-      active: true,
-    },
-    "title description price slug tags"
-  )
+  repository
+    .getBySlug(req.params.slug)
     .then((data) => {
       res.status(200).send(data);
     })
@@ -36,7 +28,8 @@ exports.getBySlug = (req, res, next) => {
 };
 
 exports.getById = (req, res, next) => {
-  Product.findById(req.params.id)
+  repository
+    .getById(req.params.id)
     .then((data) => {
       res.status(200).send(data);
     })
@@ -46,27 +39,32 @@ exports.getById = (req, res, next) => {
 };
 
 exports.getByTag = (req, res, next) => {
-  Product.find(
-    {
-      tags: req.params.tag,
-      active: true,
-    },
-    "title description price slug tags"
-  )
+  repository
+    .getByTag(req.params.tag)
     .then((data) => {
       res.status(200).send(data);
-    })
-    .catch((e) => {
+    }).catch((e) => {
       res.status(400).send(e);
     });
 };
 
 exports.post = (req, res, next) => {
-
   let contract = new ValidationContract();
-  contract.hasMinLen(req.body.title, 3, 'O título deve ter, no mínimo, 3 caracteres');
-  contract.hasMinLen(req.body.slug, 3, 'O slug deve ter, no mínimo, 3 caracteres');
-  contract.hasMinLen(req.body.description, 3, 'A descricão deve ter, no mínimo, 3 caracteres');
+  contract.hasMinLen(
+    req.body.title,
+    3,
+    "O título deve ter, no mínimo, 3 caracteres"
+  );
+  contract.hasMinLen(
+    req.body.slug,
+    3,
+    "O slug deve ter, no mínimo, 3 caracteres"
+  );
+  contract.hasMinLen(
+    req.body.description,
+    3,
+    "A descricão deve ter, no mínimo, 3 caracteres"
+  );
 
   // Se não validar
   if (!contract.isValid()) {
@@ -74,9 +72,8 @@ exports.post = (req, res, next) => {
     return;
   }
 
-  var product = new Product(req.body);
-  product
-    .save()
+ repository
+    .create(req.body)
     .then((x) => {
       res.status(201).send({
         message: "Produto Cadastrado Com Sucesso!",
@@ -91,15 +88,8 @@ exports.post = (req, res, next) => {
 };
 
 exports.put = (req, res, next) => {
-  Product.findByIdAndUpdate(req.params.id, {
-    $set: {
-      title: req.body.title,
-      description: req.body.description,
-      price: req.body.price,
-      slug: req.body.slug,
-      tags: req.body.tags,
-    },
-  })
+  repository
+    .update(req.params.id, req.body)
     .then((x) => {
       res.status(200).send({
         message: "Produto Atualizado Com Sucesso",
@@ -114,8 +104,9 @@ exports.put = (req, res, next) => {
 };
 
 exports.delete = (req, res, next) => {
-  Product.findOneAndRemove(req.body.id)
-    .then((x) => {
+repository
+  .delete(req.body.id)
+  .then((x) => {
       res.status(200).send({
         message: "Produto Removido Com Sucesso",
       });
